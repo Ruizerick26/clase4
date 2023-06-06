34d886e5d9f7d08ea104137c69a2a80fdc6bfd6c
Ruizerick26 <ruizerick2525@gmail.com>
2023-06-06 07:40:01 -0500

    primera Programacion de la clase figuras 2d

--- src/figuras_2d.java
+++ src/figuras_2d.java
@@ -1,3 +1,34 @@
 public class figuras_2d {
+    //areas y perimetros
 
+    //atributos
+    int nlados;
+    float lado;
+//CONSTRUCTOR
+    public figuras_2d(int nlados,float lado) {
+        this.nlados = nlados;
+        this.lado = lado;
+    }
+//SETTER Y GETTER DE lado
+    public float getLado() {
+        return lado;
+    }
+
+    public void setLado(float lado) {
+        this.lado = lado;
+    }
+//SETTER Y GETTER DE nlados
+    public int getNlados() {
+        return nlados;
+    }
+
+    public void setNlados(int nlados) {
+        this.nlados = nlados;
+    }
+//FUNCIONES
+    public float areacuadrado(){
+    }
+    private float perimetrocuadrado(){
+
+    }
 }
